37bdbe3131cc2213e764d42f59cf5e3080f50d9e
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2026-07-16 17:32:15 +0200

    complete property-based tests in SetADTCheck

--- src/test/scala/lab04/SetADTCheck.scala
+++ src/test/scala/lab04/SetADTCheck.scala
@@ -1,6 +1,6 @@
 package scala.lab04
 
-import org.scalacheck.Prop.forAll
+import org.scalacheck.Prop.{forAll, propBoolean}
 import org.scalacheck.{Arbitrary, Gen, Properties}
 
 import scala.lab04.SetADTs.{BasicSetADT, SetADT}
@@ -11,6 +11,7 @@ abstract class SetADTCheck(name: String) extends Properties(name):
 
   // generating a small Int
   def smallInt(): Gen[Int] = Gen.choose(0, 10)
+
   // generating a Set of Int with approximate size (modulo clashes)
   def setGen[A: Arbitrary](size: Int): Gen[Set[A]] =
     if size == 0
@@ -19,6 +20,7 @@ abstract class SetADTCheck(name: String) extends Properties(name):
       a <- Arbitrary.arbitrary[A]
       s <- setGen(size - 1)
     yield s.add(a)
+
   // a given instance to generate sets with small size
   given arb: Arbitrary[Set[Int]] = Arbitrary:
     for
@@ -26,37 +28,120 @@ abstract class SetADTCheck(name: String) extends Properties(name):
       s <- setGen[Int](i)
     yield s
 
+  /**
+   * axioms defining contains based on empty/add:
+   * contains(empty, x) = false
+   * contains(add(x,s), y) = (x == y) || contains(s, y)
+   */
+  property("axioms for contains") =
+    forAll: (s: Set[Int], x: Int, y:Int) =>
+      s.add(x).contains(y) == (x == y) || s.contains(y)
+    &&
+    forAll: (x: Int) =>
+      !empty().contains(x)
+
+  /**
+   * axioms defining remove based on empty/add:
+   * - remove(x, empty) = empty
+   * - remove(x, add(x, s)) = remove(x, s)
+   * - remove(x, add(y, s)) = add(y, remove(x, s)) if x!=y
+   */
+  property("axioms for remove") =
+    forAll: (x: Int) =>
+      empty().remove(x) === empty()
+    &&
+    forAll: (s: Set[Int], x: Int) =>
+      s.add(x).remove(x) === s.remove(x)
+    &&
+    forAll: (s: Set[Int], x: Int, y: Int) =>
+      (x != y) ==> (s.add(y).remove(x) === s.remove(x).add(y))
+
+  /**
+   * axioms defining size based on add/remove:
+   * - size(add(x, s)) = if contains(s, x) then size(s) else size(s) + 1
+   * - size(remove(x, s)) = if contains(s, x) then size(s) - 1 else size(s)
+   */
+  property("axioms for size") =
+    forAll: (s: Set[Int], x: Int) =>
+      s.add(x).size() == (if s.contains(x) then s.size() else s.size() + 1)
+    &&
+    forAll: (s: Set[Int], x: Int) =>
+      s.remove(x).size() == (if s.contains(x) then s.size() - 1 else s.size())
+
+  /** algebraic properties for union:
+   * - commutativity: union(s1, s2) = union(s2, s1)
+   * - associativity: union(union(s1, s2), s3) = union(s1, union(s2, s3))
+   * - idempotence: union(s, s) = s
+   * - the empty set is the identity element: union(s, empty) = s
+   */
+
   property("commutativity of union") =
     forAll: (s1: Set[Int], s2: Set[Int]) =>
       (s1 || s2) === (s2 || s1)
 
-  /**
-    * axioms defining contains based on empty/add:
-    * contains(empty, x) = false
-    * contains(add(x,s), y) = (x == y) || contains(s, y)
-  */
+  property("associativity of union") =
+    forAll: (s1: Set[Int], s2: Set[Int], s3: Set[Int]) =>
+      ((s1 || s2) || s3) === (s1 || (s2 || s3))
 
-  property("axioms for contains") =
-     forAll: (s: Set[Int], x: Int, y:Int) =>
-        s.add(x).contains(y) == (x == y) || s.contains(y)
-   &&
-     forAll: (x: Int) =>
-        !empty().contains(x)
+  property("idempotence of union") =
+    forAll: (s: Set[Int]) =>
+      (s || s) === s
 
-/**
- * axioms defining union and remove:
- * union(empty, s) = s
- * union(add(x, s2), s) = add(x, union(s2, s)
- * remove(x, empty) = empty
- * remove(x, add(x, s)) = remove(x, s)
- * remove(x, add(y, s)) = add(y, remove(x, s)) if x!=y
- *
- * and so on: write axioms and correspondingly implement checks
- */
+  property("the empty set is the identity element for union") =
+    forAll: (s: Set[Int]) =>
+      (s || empty()) === s
+
+  /** algebraic properties for intersection:
+   * - commutativity: intersection(s1, s2) = intersection(s2, s1)
+   * - associativity: intersection(intersection(s1, s2), s3) = intersection(s1, intersection(s2, s3))
+   * - idempotence: intersection(s, s) = s
+   * - the empty set is the absorbing element: intersection(s, empty) = empty
+   */
+
+  property("commutativity of intersection") =
+    forAll: (s1: Set[Int], s2: Set[Int]) =>
+      (s1 && s2) === (s2 && s1)
+
+  property("associativity of intersection") =
+    forAll: (s1: Set[Int], s2: Set[Int], s3: Set[Int]) =>
+      ((s1 && s2) && s3) === (s1 && (s2 && s3))
+
+  property("idempotence of intersection") =
+    forAll: (s: Set[Int]) =>
+      (s && s) === s
+
+  property("the empty set is the absorbing element for intersection") =
+    forAll: (s: Set[Int]) =>
+      (s && empty()) === empty()
+
+  /** cross-properties between union and intersection:
+   * - distributivity of intersection over union:
+   *     intersection(s1, union(s2, s3)) = union(intersection(s1, s2), intersection(s1, s3)
+   * - distributivity of union over intersection:
+   *     union(s1, intersection(s2, s3)) = intersection(union(s1, s2), union(s1, s3)
+   * - absorption laws:
+   *     union(s1, intersection(s1, s2)) = s1
+   *     intersection(s1, union(s1, s2)) = s1
+   */
+
+  property("distributivity of intersection over union") =
+    forAll: (s1: Set[Int], s2: Set[Int], s3: Set[Int]) =>
+      (s1 && (s2 || s3)) === ((s1 && s2) || (s1 && s3))
+
+  property("distributivity of union over intersection") =
+    forAll: (s1: Set[Int], s2: Set[Int], s3: Set[Int]) =>
+      (s1 || (s2 && s3)) === ((s1 || s2) && (s1 || s3))
+
+  property("absorption laws") =
+    forAll: (s1: Set[Int], s2: Set[Int]) =>
+      (s1 || (s1 && s2)) === s1
+    &&
+    forAll: (s1: Set[Int], s2: Set[Int]) =>
+      (s1 && (s1 || s2)) === s1
 
 
 object BasicSetADTCheck extends SetADTCheck("SequenceBased Set"):
   val setADT: SetADT = BasicSetADT
 
-  @main def visuallingCheckArbitrarySets =
+  @main def visualizingCheckArbitrarySets(): Unit =
     Range(0,20).foreach(i => println(summon[Arbitrary[setADT.Set[Int]]].arbitrary.sample))
